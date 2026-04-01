f09def985ab0d4d4d4bee9d586eb0943b68cc4fa
mdanna015 <m.danna1115@gmail.com>
2026-04-01 11:43:43 -0600

    Update fotos.js

--- js/fotos.js
+++ js/fotos.js
@@ -1,45 +1,7 @@
-let deletedPhotos = [];
-
 function showGallery(tab) {
   document.querySelectorAll('.gallery-tab')
     .forEach(t => t.classList.add('hidden'));
 
   document.getElementById(tab)
     .classList.remove('hidden');
-}
-
-function openPhoto(src) {
-  document.getElementById("photoViewer").classList.remove("hidden");
-  document.getElementById("fullPhoto").src = src;
-}
-
-function closePhoto() {
-  document.getElementById("photoViewer").classList.add("hidden");
-}
-
-function moveToDeleted(src) {
-  deletedPhotos.push(src);
-  alert("Movida a eliminados");
-  renderDeleted();
-}
-
-function renderDeleted() {
-  const container = document.getElementById("deleted");
-  container.innerHTML = "<p>🗑 Eliminados</p>";
-
-  deletedPhotos.forEach(src => {
-    container.innerHTML += `<img src="${src}" onclick="openPhoto('${src}')">`;
-  });
-}
-
-const hiddenCode = "1105";
-
-function openHidden() {
-  let code = prompt("Código requerido");
-
-  if(code === hiddenCode) {
-    document.getElementById("hiddenContent").classList.remove("hidden");
-  } else {
-    alert("Código incorrecto");
-  }
 }
