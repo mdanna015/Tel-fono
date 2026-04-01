9a6d8dfab3a07b355d1a77cf67c6c74bfb7fa85d
mdanna015 <m.danna1115@gmail.com>
2026-03-31 22:41:08 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+function show(screen) {
+  document.querySelectorAll('.screen').forEach(s => {
+    s.classList.remove('active');
+    s.classList.add('hidden');
+  });
 
+  document.getElementById(screen).classList.remove('hidden');
+  document.getElementById(screen).classList.add('active');
+}
+
+function openApp(app) {
+  show(app);
+}
