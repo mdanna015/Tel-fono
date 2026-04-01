1dffad8b89709956a7c2c9f4a04a1b08537e1f50
mdanna015 <m.danna1115@gmail.com>
2026-04-01 09:06:10 -0600

    Update fotos.js

--- js/fotos.js
+++ js/fotos.js
@@ -9,7 +9,22 @@ function openPhoto(src) {
   document.getElementById("fullPhoto").src = src;
   openApp('photoViewer');
 }
+let deletedPhotos = [];
 
+function moveToDeleted(src) {
+  deletedPhotos.push(src);
+  alert("Movida a eliminados");
+  renderDeleted();
+}
+
+function renderDeleted() {
+  const container = document.getElementById("deleted");
+  container.innerHTML = "<h4>🗑 Eliminados</h4>";
+
+  deletedPhotos.forEach(src => {
+    container.innerHTML += `<img src="${src}" onclick="openPhoto('${src}')">`;
+  });
+}
 function closePhoto() {
   openApp('photos');
 }
