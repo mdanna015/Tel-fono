2ae730d485f935ee8417ce27d4235417f6e55e32
mdanna015 <m.danna1115@gmail.com>
2026-03-31 22:43:20 -0600

    Update whatsapp.js

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -1,1 +1,8 @@
+function showTab(tab) {
+  document.querySelectorAll('.tab').forEach(t => t.classList.add('hidden'));
+  document.getElementById(tab).classList.remove('hidden');
+}
 
+function openProfile() {
+  openApp('profile');
+}
