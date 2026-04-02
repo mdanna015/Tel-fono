f7e8918117af9bf3e25d087283a18011ee310a1a
mdanna015 <m.danna1115@gmail.com>
2026-04-01 21:53:18 -0600

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,1 +1,11 @@
+function openApp(appId) {
+  document.querySelectorAll('.screen')
+    .forEach(s => {
+      s.classList.remove('active');
+      s.classList.add('hidden');
+    });
 
+  const app = document.getElementById(appId);
+  app.classList.remove('hidden');
+  app.classList.add('active');
+}
